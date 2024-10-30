cd3eb1a4f121740fe03ce8422ac827f048db517b
Carolina <79524656+carochacs@users.noreply.github.com>
2024-10-30 13:29:23 -0600

    Added disable function

--- Firmware/Marlin/Configuration.h
+++ Firmware/Marlin/Configuration.h
@@ -1651,9 +1651,9 @@
 
 // Disable axis steppers immediately when they're not being stepped.
 // WARNING: When motors turn off there is a chance of losing position accuracy!
-//#define DISABLE_X
-//#define DISABLE_Y
-//#define DISABLE_Z
+#define DISABLE_X
+#define DISABLE_Y
+#define DISABLE_Z
 //#define DISABLE_I
 //#define DISABLE_J
 //#define DISABLE_K
